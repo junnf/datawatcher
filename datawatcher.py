043b0a1dc5f6f016ec39f73923064e5384b67820
junn <junnflow@gmail.com>
2015-10-20 14:39:43 +0800

    test-message

--- datawatcher.py
+++ datawatcher.py
@@ -7,25 +7,48 @@ import traceback
 import os
 import sys
 import sched
+
 from setting.setting import host, dbname, dbuser, password 
+#from setting.setting import recommend_member
+
+from sendcloud import sendemail
+from sendcloud import sendmessage
 
 class Db(object):
     
     def __init__(self):
+        
         try:
-            db = mysqldb.Connection(host,dbname,dbuser,password)
-        except Exception, e:
-            print traceback.print_exc()
-            
-    def membersmall5(self):
-        try:  
-            _db_data = self.db.query('select * from bussiness_member_recommend  \
-            group by customer_id having count(*) <= 5')
+            self.db = mysqldb.Connection(host,dbname,dbuser,password)
         except Exception, e:
             print traceback.print_exc()
             return
-        return _db_data
-        
+
+    def membersmall5(self,exist_recommend):
+        '''
+          需要重新考虑完全没有推荐的人
+          _db_data2部分
+        '''
+        try:  
+            _db_data1 = self.db.query('select name,telephone,email      \
+                from customer_member_t where is_open_recommend = 1      \
+                and resume_review_status=10 and member_id in            \
+                (select customer_id from bussiness_member_recommend     \
+                group by customer_id having count(*) <= 5) order by member_id;')
+
+            _db_data2 = self.db.query('select name,email,telephone from \
+                customer_member_t where member_id not in \
+                (select customer_id from bussiness_member_recommend )   \
+                and  is_open_recommend = 1 and resume_review_status=10;')
+        except Exception, e:
+            print traceback.print_exc()
+            return
+
+        if exist_recommend == 1:
+            return _db_data1
+        elif exist_recommend == 0:
+            return _db_data2
+
     def memberx3d5(self):
         '''
           invitation < 3, recommend > 5
@@ -34,6 +57,11 @@ class Db(object):
             _db_data = self.db.query('select customer_id from bussiness_member_recommend where  \
             customer_id in (select customer_id from positioninv group by customer_id     \
             having count(*) < 3) group by customer_id having count(*) >= 5;')
+            #print _db_data
+            for x in _db_data:
+                print self.db.query('select * from bussiness_member_recommend where customer_id = ' + str(x['customer_id']))
+                print '\n'
+
         except Exception, e:
             print traceback.print_exc()
             return
@@ -114,13 +142,14 @@ class Db(object):
             #
             #_db_data = self.db.query('select inv_id, customer_id,  \ 
             #completed_at, status from positioninv where status = 0')
-            _db_data1 = self.db.query('select inv_id,customer_id,   \
+            _db_data1 = self.db.query('select inv_id, customer_id   \
             from positioninv where status = 0 and UNIX_TIMESTAMP()  \
             - completed_at > 172800  ')
+
         except Exception, e:
             print traceback.print_exc()
             return
-        return _db_data
+        return _db_data1
 
     def c_extremity_no_feedback_close_recommend(self):
 
@@ -130,13 +159,53 @@ class Db(object):
         try :
             _db_data - self.db.query('select inv_id,customer_id,from positioninv \
                 where status = 0 and UNIX_TIMESTAMP() - completed_at > 259200')
+        
         except Exception, e:
             print traceback.print_exc()
             return
+        
         return _db_data
             
+def test_1():
+    db = Db()
+    member1 = db.memberx3d5()
+    for x in member1:
+        
+      member2 = dm.memberx3x5()
+    
+
+def test_danger():
+    db = Db() 
+    member0 = db.membersmall5(0)
+    member1 = db.membersmall5(1)
+    for y in member0: 
+        sendemail.get_params(emailto = str(x['email'])),
+        emailfrom = 'datacenter@sendcloud.org',
+        fromname = 'DataCenter',
+        subject = '请修改条件，修改职位描述'
+        content = x['name']+'你好: 你的职位推荐有问题,请修改'
+    
+    for x in member1:
+        sendemail.get_params(emailto = str(x['email'])),
+        emailfrom = 'datacenter@sendcloud.org',
+        fromname = 'DataCenter',
+        subject = '请修改条件，修改职位描述'
+        content = x['name']+'你好: 你的职位推荐有问题,请修改' 
+   
+    member2 = db.memberx3d5()
+    
+       # sendemail.get_params(emailto = str())    
+
 def test():
-    pass
+    db = Db()
+    #db.memberx3d5()
+    mem0 = db.membersmall5(0)
+    mem1 = db.membersmall5(1)
+    for x in mem0:
+        _name = x['name'].encode('utf-8')
+        _email = x['email'].encode('utf-8')
+        _tel = x['telephone'].encode('utf-8')
+
 
 def task():
     
@@ -146,15 +215,17 @@ def task():
     #continue_recommend(member1)
     continue_member1 = db.membersmall5()
     for n1_member in continue_member1:
+        print n1_member
         '''
-            notify module
+        #    notify module
         '''
         #notify
 
     member2 = db.memberx3d5()
     for n2_member in member2:
+        print n2_member
         '''
-            无法解决
+       #     无法解决
         '''
     member3 = db.memberx3x5()
     #continue_recommend(member3)
@@ -164,9 +235,12 @@ def task():
     member5 = db.b_extremity_no_feedback()
         #notify b-member
     #_now_time2 = time.time()
-
+    
 if __name__ == '__main__':
-#   task start
-    task()
+  
+  # db = mysqldb.Connection(host,dbname,dbuser,password)
+ #   test()
+  #   task start
+    test()
 
  
